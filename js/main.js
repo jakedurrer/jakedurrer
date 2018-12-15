9063517f5eaa2f6514de04bf54a353950f2533ab
Jake Durrer <jake.durrer@gmail.com>
2018-12-14 19:26:50 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+
 //Set Initial State of Menu
 let showMenu = false; 
 
@@ -29,3 +30,24 @@ function toggleMenu(){
         showMenu = false;
     }
 }
+
+$('#id4, #id5').hover(function(event) {
+    if(event.type === "mouseenter") {
+        $(this).attr("controls", "");
+        console.log("shalom");
+    } else if(event.type === "mouseleave") {
+        $(this).removeAttr("controls");
+    }
+});
+
+$(".button").on("click", function() {
+    var modal = $(this).data("modal");
+    $(modal).show();
+  });
+  
+$(".modal").on("click", function(e) {
+    var className = e.target.className;
+    if(className === "modal" || className === "close"){
+      $(this).closest(".modal").hide();
+    }
+});
